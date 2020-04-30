7344456cf03bda84dd7f3b6812e20c68af4103e3
Jazzz-dev <63164006+Jazzz-dev@users.noreply.github.com>
2020-05-01 04:55:29 +0600

    js modal login

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+
+var open = document.querySelector(".login-link");
+var popup = document.querySelector(".modal-login");
+var close = document.querySelector(".modal-close");
+var vv = document.querySelector(".contacts");
+var open2 = vv.querySelector("[href = map.html]");
+var popup2 = document.querySelector(".modal-map");
+var close2 = document.querySelector(".modal-close");
+var overlay = document.querySelector(".overlay-off");
+
+open.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  overlay.classList.add("overlay-on");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  overlay.classList.remove("overlay-on");
+});
+
+open2.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup2.classList.add("modal-show");
+  overlay.classList.add("overlay-on");
+});
+
+close2.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup2.classList.remove("modal-show");
+  overlay.classList.remove("overlay-on");
+});
